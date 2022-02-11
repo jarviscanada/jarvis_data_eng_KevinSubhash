d554d0b1cc6c709cb2b81693fee968608cc999e9
Kevin Subhash <kevinsubhash@gmail.com>
2022-02-11 18:16:19 +0000

    Minor fixes

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -18,13 +18,13 @@ cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" | awk '{print $3" "$4" "$5" "$6" "$7}' | xargs)
 cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print substr($3, 1, length($3)-1)}' | xargs)
 total_mem=$(echo `cat /proc/meminfo` | egrep "MemTotal:" | awk '{print $2}' | xargs)
 timestamp= $(echo `vmstat -t` | awk '{print $43" "$44}' | xargs)
 
-insert_stmt="INSERT INTO host_info(id, hostname, cpu_number, cpu_architecture, cpu_model,
-             	cpu_mhz, L2_cache, total_mem, timestamp) VALUES ('$id', '$hostname', '$cpu_number',
-             	'$cpu_architecture', '$cpu_model', '$cpu_mhz', '$L2_cache', '$total_mem', '$timestamp')"
+insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model,
+             	cpu_mhz, L2_cache, total_mem, timestamp) VALUES ('$hostname', '$cpu_number',
+             	'$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp')"
 
 export PGPASSWORD=$psql_password
 
